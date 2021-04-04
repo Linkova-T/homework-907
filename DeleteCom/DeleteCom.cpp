6fc364b46a62d5174bc1cd88627e9b5c06c7bb24
Linkova Tanya <Linkova Tanya@DESKTOP-BGMEJG7>
2021-04-04 07:41:24 +0300

    Add project files.

---
+++ DeleteCom/DeleteCom.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+
+int main() {
+
+    std::fstream ofile("output.txt", std::ios::out);
+    std::fstream ifile("input.txt", std::ios::in);
+    std::string str;
+    bool flag = true;
+    while (!ifile.eof()) {
+        getline(ifile, str);
+        for (int i = 0; i < str.length(); ++i) {
+           
+                if ((str[i] == '/') && (str[i + 1] == '/')) { ofile << '\n'; break; }
+                if ((str[i] == '/') && (str[i + 1] == '*')) { flag = false; }
+                if ((str[i] == '*') && (str[i + 1] == '/')) { i += 2; flag = true; }
+                if (flag) { ofile << str[i]; }
+            
+        }
+    }
+    ifile.close();
+
+    return 0;
+}
+
